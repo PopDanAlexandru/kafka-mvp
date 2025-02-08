7a6a72d9b56b13caef458a07ead26957039ebae1
Pop Dan Alexandru <43861197+PopDanAlexandru@users.noreply.github.com>
2025-02-08 20:14:59 +0200

    Added batch processing configuration

--- src/main/java/mvp/kafka/consumer/KafkaConsumerService.java
+++ src/main/java/mvp/kafka/consumer/KafkaConsumerService.java
@@ -1,13 +1,19 @@
 package mvp.kafka.consumer;
 
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = "demo-topic", groupId = "demo-group")
-    public void consume(String message) {
-        System.out.println("Consumed message: " + message);
+    @KafkaListener(topics = "demo-topic",
+                   groupId = "demo-group",             // connect to a specific broker and topic
+                   containerFactory = "batchFactory")  // configure the consumption behavior (batches, polling interval)
+    public void consume(List<String> messages) {
+        log.info("Consumed a batch of {} messages", messages.size());
+        messages.forEach(msg -> log.info("Message: {}", msg));
     }
 }
